4773fb7f674eceeff6163567f9da68a2978c27d9
Daria <daria7ztv@gmail.com>
2022-10-18 17:59:41 +0300

    car table

--- app/db/db_functions.py
+++ app/db/db_functions.py
@@ -30,10 +30,52 @@ def get_last_trans_id() -> int:
     with cur as cur:
         cur.execute(f"SELECT transaction_id FROM fact_payments ORDER BY transaction_id DESC LIMIT 1")
         trans_id = cur.fetchone()
-        print(trans_id[0])
     cur.close()
     conn.close()
     return 0 if trans_id is None else trans_id[0]
+
+
+def check_car_record_exists(plate_num) -> bool:
+    conn = create_conn()
+    cur = conn.cursor()
+    with cur as cur:
+        cur.execute(f"SELECT plate_num FROM dim_cars WHERE plate_num LIKE '{plate_num}' ORDER BY revision_dt DESC LIMIT 1")
+        trans_id = cur.fetchone()
+        # print(trans_id[0])
+    cur.close()
+    conn.close()
+    return False if trans_id is None else True
+
+
+def get_all_cars():
+    conn = create_conn()
+    cur = conn.cursor()
+    with cur as cur:
+        cur.execute(f"SELECT * FROM dim_cars")
+        print(cur.fetchall())
+    cur.close()
+    conn.close()
+
+def get_all_clients():
+    conn = create_conn()
+    cur = conn.cursor()
+    with cur as cur:
+        cur.execute(f"SELECT * FROM dim_clients")
+        print(cur.fetchall())
+    cur.close()
+    conn.close()
+
+
+def check_client_card_changed(client_phone: str) -> bool | str:
+    conn = create_conn()
+    cur = conn.cursor()
+    with cur as cur:
+        cur.execute(f"SELECT phone_num, card_num FROM dim_clients WHERE phone_num LIKE '{client_phone.strip()}' ORDER BY start_dt DESC LIMIT 1")
+        client_record = cur.fetchone()
+        # print(trans_id[0])
+    cur.close()
+    conn.close()
+    return False if client_record is None else client_record[1]
 
 
 def add_data_to_payments(df: list, table: str):
@@ -50,4 +92,111 @@ def add_data_to_payments(df: list, table: str):
         conn.rollback()
         cur.close()
     cur.close()
+    return
+
+
+def add_data_to_cars(df: list):
+    if not df.empty:
+        conn = create_conn()
+        cur = conn.cursor()
+        for index, row in df.iterrows():
+            # print(row['finished_flag'])
+            if not check_car_record_exists(row['plate_num']):
+                print('hi')
+                insert_query = """
+                            INSERT INTO  dim_cars (plate_num, start_dt, model_name, revision_dt, deleted_flag, end_dt)
+                            VALUES (%s,%s,%s,%s,%s,%s)
+                            """
+                record_to_insert = (
+                    row['plate_num'],
+                    row['update_dt'],
+                    row['model'].strip(),
+                    row['revision_dt'],
+                    row['finished_flag'],
+                    None,
+                )
+                cur.execute(insert_query, record_to_insert)
+            else:
+                print("!")
+                update_query = """
+                               UPDATE  dim_cars
+                               SET end_dt = %s
+                               WHERE plate_num = %s
+                               ORDER BY revision_dt DESC
+                               LIMIT 1
+                               """
+                cur.execute(update_query, (row['update_dt'], row['plate_num']))
+                insert_query = """
+                               INSERT INTO  dim_cars (plate_num, start_dt, model_name, revision_dt, deleted_flag, end_dt)
+                               VALUES (%s,%s,%s,%s,%s,%s)
+                               """
+                record_to_insert = (
+                    row['plate_num'],
+                    row['update_dt'],
+                    row['model'].strip(),
+                    row['revision_dt'],
+                    row['finished_flag'],
+                    None,
+                )
+                cur.execute(insert_query, record_to_insert)
+        try:
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            conn.rollback()
+            cur.close()
+        cur.close()
+    return
+
+
+def add_data_to_clients(df: list):
+    if not df.empty:
+        conn = create_conn()
+        cur = conn.cursor()
+        for index, row in df.iterrows():
+            # print(row['finished_flag'])
+            db_card = check_client_card_changed(row['client_phone'])
+            if not db_card:
+                # print('hi')
+                insert_query = """
+                            INSERT INTO  dim_clients (phone_num, start_dt, card_num, deleted_flag, end_dt)
+                            VALUES (%s,%s,%s,%s,%s)
+                            """
+                record_to_insert = (
+                    row['client_phone'].strip(),
+                    row['dt'],
+                    row['card_num'].strip(),
+                    'N',
+                    None,
+                )
+                cur.execute(insert_query, record_to_insert)
+            elif row['card_num'] != db_card:
+                print("!")
+                # update_query = """
+                #                UPDATE  dim_clients
+                #                SET end_dt = %s
+                #                WHERE phone_num = %s
+                #                ORDER BY start_dt DESC
+                #                LIMIT 1
+                #                """
+                # cur.execute(update_query, (row['dt'], row['client_phone']))
+                # insert_query = """
+                #             INSERT INTO  dim_clients (phone_num, start_dt, card_num, deleted_flag, end_dt)
+                #             VALUES (%s,%s,%s,%s,%s)
+                #             """
+                # record_to_insert = (
+                #     row['client_phone'].strip(),
+                #     row['dt'],
+                #     row['card_num'].strip(),
+                #     False,
+                #     None,
+                # )
+                # cur.execute(insert_query, record_to_insert)
+        try:
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            conn.rollback()
+            cur.close()
+        cur.close()
     return
--- app/db/load_table_data.py
+++ app/db/load_table_data.py
@@ -1,19 +1,68 @@
 import psycopg2
+import pandas as pd
+
+from db_functions import add_data_to_cars, get_all_cars, add_data_to_clients, get_all_clients
 
 
-shema = 'main'
+def create_conn() -> str:
+    shema = 'main'
 
-conn = psycopg2.connect(
-    host='de-edu-db.chronosavant.ru',
-    port=5432,
-    database='taxi',
-    user='etl_tech_user',
-    password='etl_tech_user_password',
-    options=f'-c search_path={shema}',
-)
-cur = conn.cursor()
-with cur as cur:
-    cur.execute('SELECT * FROM drivers')
-    print(cur.fetchall())
-cur.close()
-conn.close()
+    conn = psycopg2.connect(
+        host='de-edu-db.chronosavant.ru',
+        port=5432,
+        database='taxi',
+        user='etl_tech_user',
+        password='etl_tech_user_password',
+        options=f'-c search_path={shema}',
+    )
+    return conn
+
+
+def load_new_car_records():
+    conn  = create_conn()
+    cur = conn.cursor()
+    with cur as cur:
+        cur.execute("SELECT * FROM car_pool WHERE DATE(update_dt) = (CURRENT_DATE - INTEGER '1')")
+        query = cur.fetchall()
+    df = pd.DataFrame(query, columns= ['plate_num', 'model', 'revision_dt', 'register_dt', 'finished_flag', 'update_dt'])
+    #cur.close()
+    conn.close()
+    #print(type(df))
+    df['revision_dt'] = df['revision_dt'].astype('datetime64[ns]')
+    df['register_dt'] = df['register_dt'].astype('datetime64[ns]')
+    df['update_dt'] = df['update_dt'].astype('datetime64[ns]')
+    return df
+
+
+def load_new_client_records():
+    conn  = create_conn()
+    cur = conn.cursor()
+    with cur as cur:
+        cur.execute("SELECT * FROM rides WHERE DATE(dt) = (CURRENT_DATE - INTEGER '6')")
+        query = cur.fetchall()
+    df = pd.DataFrame(query, columns= [
+        'ride_id',
+        'dt',
+        'client_phone',
+        'card_num',
+        'point_from',
+        'point_to',
+        'distance',
+        'price',
+    ])
+    #cur.close()
+    conn.close()
+    #print(type(df))
+    df['dt'] = df['dt'].astype('datetime64[ns]')
+    return df
+
+
+if __name__ == "__main__":
+    # df = load_new_car_records()
+    # print(df)
+    # add_data_to_cars(df=df)
+    # get_all_cars()
+    df = load_new_client_records()
+    print(df)
+    #add_data_to_clients(df=df)
+    get_all_clients()
